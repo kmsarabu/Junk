ce240106ef07a1188005450f3e8fed26fbf677ea
kmsarabu <34084857+kmsarabu@users.noreply.github.com>
2018-10-23 16:00:14 -0400

    Update megaball_ticket_gen.py

--- megaball_ticket_gen.py
+++ megaball_ticket_gen.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 
-from multiprocessing import Process, Value, Array
+from multiprocessing import Process, Manager
 from random import shuffle,randint
-from ctypes import c_char_p
 import time
 import argparse,sys,os
 
@@ -11,47 +10,55 @@ parser.add_argument('-t', dest='tickets', type=int, default=1, help='Number of T
 
 args = parser.parse_args()
 tickets=1
-def shuffle_balls(vB):
-        Ctr=0
-        while (True):
-                shuffle(vB)
-                time.sleep(0.01)
 
-def popb(vB, fval):
-        Ctr=1
-        values=""
-        BallsCnt=len(vB)
-        while (Ctr <= 5):
-                rgen=randint(1, int(BallsCnt))-1
-                rlist=list(vB)
-                rval=rlist.pop(rgen)
-                vB=Array('i', rlist)
-                values=values + " " + str(rval)
-                BallsCnt=BallsCnt-1
-                Ctr=Ctr+1
-                time.sleep(1)
-        #fval.value=StringVar(values)
-        print ("Ticket " + values)
+def shuffle_balls(vB):
+ try:
+	Ctr=0
+	while (True):
+		try:
+			shuffle(vB)
+		except Exception, e:
+			pass
+		time.sleep(0.001)
+ except Exception, e:
+	print ("Error in shuffleballs")
+	print (len(vB))
+	
+def popb(vB, Res):
+	Ctr=0
+	values=""
+	BallsCnt=len(vB)
+	while (Ctr < 5):
+		rgen=randint(1, int(BallsCnt))-1
+		rval=vB.pop(rgen)
+		Res[Ctr]=rval
+		values=values + " " + str(rval)
+		BallsCnt=BallsCnt-1
+		Ctr=Ctr+1
+		time.sleep(1)
 
 while (tickets <= args.tickets):
 
-        Balls=Array('i', range(1,71))
-        MBalls=Array('i', range(1,26))
+	with Manager() as manager:
+		Balls = manager.list( range(1,71) )
+		MBalls = manager.list( range(1,26) )
+		Res = manager.list( range(1,7) )
 
-        final_nums=Value(c_char_p, b"")
-        p1 = Process(target=shuffle_balls,args=(Balls,))
-        p1.start()
-        p2 = Process(target=shuffle_balls,args=(MBalls,))
-        p2.start()
-        time.sleep(2)
-        p = Process(target=popb,args=(Balls, final_nums,))
-        p.start()
+		p1 = Process(target=shuffle_balls,args=(Balls,))
+		p1.start()
+		p2 = Process(target=shuffle_balls,args=(MBalls,))
+		p2.start()
+		time.sleep(2)
+		p = Process(target=popb,args=(Balls, Res,))
+		p.start()
 
-        p.join()
+		p.join()
 
-        #print ("MegaBall: ")
-        #print (MBalls[randint(1, 25)-1])
-        fv = "---------> MegaB: "  + str(MBalls[randint(1, 25)-1])
-        print (fv)
-        p1.terminate()
-        p2.terminate()
+		ri = randint(1, 25)-1
+		rval = MBalls[ri]
+		Res[5] = rval
+		print (Res)
+		p1.terminate()
+		p2.terminate()
+
+		tickets=tickets+1
